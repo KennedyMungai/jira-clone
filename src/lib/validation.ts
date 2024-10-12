47ee20269aa11eafcf9a40f2a7e3c1ad8a421fe6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-12 11:36:24 +0300

    Added some more properties for the signInSchema

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().trim().min(1, "Email is required").email(),
+  password: z.string().min(8, "Password is required"),
 });
