d14e4ff9ec8538f1d16f82aa70794e2a689d4905
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-12 11:40:24 +0300

    Updated the minimum number of characters needed for the signin password to 1

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const signInSchema = z.object({
   email: z.string().trim().min(1, "Email is required").email(),
-  password: z.string().min(8, "Password is required"),
+  password: z.string().min(1, "Password is required"),
 });
