c5c05de87cb1387cf0e2a59c6375d089b556cb74
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-12 11:52:07 +0300

    Added the scheama to be used whem validating the signup form

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -4,3 +4,9 @@ export const signInSchema = z.object({
   email: z.string().trim().min(1, "Email is required").email(),
   password: z.string().min(1, "Password is required"),
 });
+
+export const signUpSchema = z.object({
+  name: z.string().trim().min(1, "Name is required"),
+  email: z.string().trim().min(1, "Email is required").email(),
+  password: z.string().min(8, "The password should be at least 8 characters"),
+});
