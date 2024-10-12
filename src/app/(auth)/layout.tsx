99dcb69e2633244f0bebf7cc674f0962a383f45a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-12 10:41:07 +0300

    Added some styling to the auth layout file

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 type Props = {
@@ -5,7 +7,19 @@ type Props = {
 };
 
 const AuthLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <main className="h-full bg-neutral-100">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex items-center justify-between">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <Button variant={"secondary"}>Sign Up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default AuthLayout;
