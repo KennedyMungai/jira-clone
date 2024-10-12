dde67f6843e5c3b154ecebad94dd4a62b9850771
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-12 11:58:46 +0300

    Dynamically updating the button text and the href link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,12 +11,18 @@ type Props = {
 };
 
 const AuthLayout = ({ children }: Props) => {
+  const pathname = usePathname();
+
   return (
     <main className="h-full bg-neutral-100">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex items-center justify-between">
           <Image src="/logo.svg" alt="logo" width={152} height={56} />
-          <Button variant={"secondary"}>Sign Up</Button>
+          <Button variant={"secondary"} asChild>
+            <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+              {pathname === "/sign-in" ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
