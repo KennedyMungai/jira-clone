833d4407e66795eaf52a9d7a070ce5116a245cc5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 10:59:47 +0300

    Logging in the current user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const HomePage = () => {
-  return <div className="space-y-4 p-4">HomePage</div>;
+  const { data, isLoading } = useCurrent();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!data && !isLoading) router.push("/sign-in");
+  }, [data, isLoading, router]);
+
+  return <div className="space-y-4 p-4">Only visible to authorized users</div>;
 };
 
 export default HomePage;
