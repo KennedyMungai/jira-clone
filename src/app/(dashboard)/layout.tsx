56f6af7e5a53aac5762caa25d3e88df87ecbf26f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 15:57:57 +0300

    Created the navbar component and added it to the dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 import { ReactNode } from "react";
 
@@ -12,9 +13,9 @@ const DashboardLayout = ({ children }: Props) => {
         <div className="fixed left-0 top-0 hidden h-full overflow-y-auto lg:block lg:w-[264px]">
           <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
-          <div className="mx-auto h-full max-w-screen-2xl">
-            {/* TODO: Navbar */}
+        <div className="w-full lg:pl-[264px]">
+          <div className="mx-auto size-full max-w-screen-2xl">
+            <Navbar />
             <main className="flex h-full flex-col px-6 py-8">{children}</main>
           </div>
         </div>
