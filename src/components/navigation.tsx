94ff487342e0820a3265dae606f18c1127dc7ad5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 14:39:58 +0300

    Added the links needed for navigation in the sidebar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,62 @@
-const Navigation = () => {
-return <div>Navigation</div>
-}
+import { cn } from "@/lib/utils";
+import { SettingsIcon, UsersIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  GoCheckCircle,
+  GoCheckCircleFill,
+  GoHome,
+  GoHomeFill,
+} from "react-icons/go";
 
-export default Navigation
+const routes = [
+  {
+    label: "Home",
+    href: "",
+    icon: GoHome,
+    activeIcon: GoHomeFill,
+  },
+  {
+    label: "My Tasks",
+    href: "/tasks",
+    icon: GoCheckCircle,
+    activeIcon: GoCheckCircleFill,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: SettingsIcon,
+    activeIcon: SettingsIcon,
+  },
+  {
+    label: "Members",
+    href: "/members",
+    icon: UsersIcon,
+    activeIcon: UsersIcon,
+  },
+];
+
+const Navigation = () => {
+  return (
+    <ul className="flex flex-col">
+      {routes.map((item) => {
+        const isActive = false;
+        const Icon = isActive ? item.activeIcon : item.icon;
+
+        return (
+          <Link key={item.label} href={item.href}>
+            <div
+              className={cn(
+                "flex items-start gap-2.5 rounded-md p-2.5 font-medium text-neutral-500 transition hover:text-primary",
+                isActive && "bg-white text-primary shadow-sm hover:opacity-100",
+              )}
+            >
+              <Icon className="mr-2 size-5 text-neutral-500" /> {item.label}
+            </div>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Navigation;
