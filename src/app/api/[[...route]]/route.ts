fc07e39228a098f0b69b106319faa539abda75dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 08:10:13 +0300

    Setup the hono auth route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import auth from "@/features/auth/server/route";
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+const app = new Hono().basePath("/api").route("/auth", auth);
 
 export const GET = handle(app);
 export const POST = handle(app);
---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { signInSchema } from "@/lib/validation";
+
+const app = new Hono().post(
+  "/login",
+  zValidator("json", signInSchema),
+  async (c) => {
+    const { email, password } = await c.req.valid("json");
+
+    return c.json({ email, password });
+  },
+);
+
+export default app;
