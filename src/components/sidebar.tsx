14580deab7a18fb995d30464cc01c6039b677c34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 14:28:07 +0300

    Created and rendered the Navigation component inside the SIdebar component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,18 @@
+import DottedSeparator from "@/components/dotted-separator";
+import Image from "next/image";
+import Link from "next/link";
+import Navigation from "@/components/navigation";
+
 const Sidebar = () => {
-  return <aside className="size-full border border-r">Sidebar</aside>;
+  return (
+    <aside className="size-full border border-r bg-neutral-100 p-4">
+      <Link href="/">
+        <Image src="/logo.svg" width={164} height={48} alt="logo" />
+      </Link>
+      <DottedSeparator className="my-4" />
+      <Navigation />
+    </aside>
+  );
 };
 
 export default Sidebar;
