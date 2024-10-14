f66882b99d681e668e3793921497b6d49bb5ac1e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 11:06:40 +0300

    Implemented the logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const HomePage = () => {
-  const { data, isLoading } = useCurrent();
-
   const router = useRouter();
+
+  const { data, isLoading } = useCurrent();
+  const { mutate, isPending } = useLogout();
 
   useEffect(() => {
     if (!data && !isLoading) router.push("/sign-in");
   }, [data, isLoading, router]);
 
-  return <div className="space-y-4 p-4">Only visible to authorized users</div>;
+  return (
+    <div className="space-y-4 p-4">
+      Only visible to authorized users
+      <Button onClick={mutate} disabled={isPending}>
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,15 +1,23 @@
 import { client } from "@/lib/hc";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout.$post>;
 type RequestType = InferRequestType<typeof client.api.auth.logout.$post>;
 
-export const useLogout = () =>
-  useMutation<ResponseType, Error, RequestType>({
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async () => {
       const response = await client.api.auth.logout.$post();
 
       return await response.json();
     },
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["current-user"] }),
   });
+
+  return mutation;
+};
+  
