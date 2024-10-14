4429736ffcb880e0f6ea2c99ce25e1d6700c2d08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 08:28:31 +0300

    Defined the useLogin hook

---
+++ src/features/auth/api/use-login.ts
@@ -0,0 +1,15 @@
+import { client } from "@/lib/hc";
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+type ResponseType = InferResponseType<typeof client.api.auth.login.$post>;
+type RequestType = InferRequestType<typeof client.api.auth.login.$post>["json"];
+
+export const useLogin = () =>
+  useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.auth.login.$post({ json });
+
+      return await response.json();
+    },
+  });
