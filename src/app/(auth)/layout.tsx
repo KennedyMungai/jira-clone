8f29310a1e9a578257387001eabc4663a2fae0ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 08:27:57 +0300

    Made the logo link back to the index page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -17,7 +17,9 @@ const AuthLayout = ({ children }: Props) => {
     <main className="h-full overflow-y-auto bg-neutral-100">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex items-center justify-between">
-          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <Link href="/">
+            <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          </Link>
           <Button variant={"secondary"} asChild>
             <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
               {pathname === "/sign-in" ? "Sign Up" : "Login"}
