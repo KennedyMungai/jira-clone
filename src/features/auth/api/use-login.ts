3409f5925d192bbe671f572948828b567f337b40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 12:15:46 +0300

    Removed the toast to be called when there is an error login in

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,7 +1,6 @@
 import { client } from "@/lib/hc";
 import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
-import { toast } from "sonner";
 
 type ResponseType = InferResponseType<typeof client.api.auth.login.$post>;
 type RequestType = InferRequestType<typeof client.api.auth.login.$post>["json"];
@@ -13,5 +12,4 @@ export const useLogin = () =>
 
       return await response.json();
     },
-    onError: (error: Error) => toast.error(error.message),
   });
