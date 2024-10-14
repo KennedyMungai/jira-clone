067dbf8de5f0c0b59f8d27ad53b48f00b6dba7cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 08:35:49 +0300

    Created the route to register a user

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,15 +1,17 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
-import { signInSchema } from "@/lib/validation";
+import { signInSchema, signUpSchema } from "@/lib/validation";
 
-const app = new Hono().post(
-  "/login",
-  zValidator("json", signInSchema),
-  async (c) => {
+const app = new Hono()
+  .post("/login", zValidator("json", signInSchema), async (c) => {
     const { email, password } = await c.req.valid("json");
 
     return c.json({ email, password });
-  },
-);
+  })
+  .post("/register", zValidator("json", signUpSchema), async (c) => {
+    const { email, name, password } = await c.req.valid("json");
+
+    return c.json({ email, name, password });
+  });
 
 export default app;
