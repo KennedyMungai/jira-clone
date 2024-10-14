e2a402c5b9336e306c894d3f56a796135bbcc89a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 12:17:44 +0300

    Removed some of the markup and logic from the page and removed the use client statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-"use client";
-
-import { useCurrent } from "@/features/auth/api/use-current";
 import UserButton from "@/features/auth/components/user-button";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 const HomePage = () => {
-  const router = useRouter();
-
-  const { data, isLoading } = useCurrent();
-
-  useEffect(() => {
-    if (!data && !isLoading) router.push("/sign-in");
-  }, [data, isLoading, router]);
-
   return (
     <div className="space-y-4 p-4">
       Only visible to authorized users
