aeb5a7f6961034b3656466ed37f5c89b4575b06d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 12:13:47 +0300

    Added an onError function to call a toast when there os an issue with loggin in

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,6 +1,7 @@
 import { client } from "@/lib/hc";
 import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<typeof client.api.auth.login.$post>;
 type RequestType = InferRequestType<typeof client.api.auth.login.$post>["json"];
@@ -12,4 +13,5 @@ export const useLogin = () =>
 
       return await response.json();
     },
+    onError: (error: Error) => toast.error(error.message),
   });
