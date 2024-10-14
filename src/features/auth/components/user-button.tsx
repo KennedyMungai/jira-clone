2d9086ce65b46e7bd98c1db0dd9ac2bce0f55b96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 12:11:11 +0300

    Added a logout button that calls the logout functionality

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import DottedSeparator from "@/components/dotted-separator";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LoaderIcon } from "lucide-react";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { LoaderIcon, LogOutIcon } from "lucide-react";
 
 const UserButton = () => {
   const { data: user, isLoading: isUserLoading } = useCurrent();
+  const { mutate: logout } = useLogout();
 
   if (isUserLoading) {
     return (
@@ -24,18 +29,48 @@ const UserButton = () => {
   const { name, email } = user;
 
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    <Avatar className="size-10 border border-neutral-300 transition hover:opacity-75">
-      <AvatarImage src="" alt="user name" />
-      <AvatarFallback className="flex items-center justify-center bg-neutral-200 font-medium text-neutral-500">
-        {name
-          ? name.charAt(0).toUpperCase()
-          : (email.charAt(0).toUpperCase() ?? "U")}
-      </AvatarFallback>
-    </Avatar>
-    //   </DropdownMenuTrigger>
-    // </DropdownMenu>
+    // The modal prop is useful incase the dropdown is rendered inside another dropdown or modal to avoid conflicts.
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild className="relative outline-none">
+        <Avatar className="size-10 border border-neutral-300 transition hover:opacity-75">
+          <AvatarFallback className="flex items-center justify-center bg-neutral-200 font-medium text-neutral-500">
+            {name
+              ? name.charAt(0).toUpperCase()
+              : (email.charAt(0).toUpperCase() ?? "U")}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        side="bottom"
+        className="w-60"
+        sideOffset={10}
+      >
+        <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+          <Avatar className="size-[52px] border border-neutral-300">
+            <AvatarFallback className="flex items-center justify-center bg-neutral-200 text-xl font-medium text-neutral-500">
+              {name
+                ? name.charAt(0).toUpperCase()
+                : (email.charAt(0).toUpperCase() ?? "U")}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name ?? "User"}
+            </p>
+            <p className="text-xs text-neutral-500">{email}</p>
+          </div>
+        </div>
+        <DottedSeparator className="mb-1" />
+        <DropdownMenuItem
+          className="flex h-10 cursor-pointer items-center justify-center font-medium text-amber-700"
+          onClick={logout}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
