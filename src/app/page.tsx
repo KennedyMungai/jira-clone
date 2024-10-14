0214b7aa08f09cd578989a1363b47a2424ae064c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-14 09:57:40 +0300

    Basic appWrite setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { MessageCircleIcon } from "lucide-react";
+"use client";
 
 const HomePage = () => {
-	return (
-		<div className="space-y-4 p-4">
-			<div className="space-x-4">
-				<Button>Primary</Button>
-				<Button variant={"secondary"}>Secondary</Button>
-				<Button variant={"outline"}>Outline</Button>
-				<Button variant={"destructive"}>Destructive</Button>
-				<Button variant={"ghost"}>Ghost</Button>
-				<Button variant={"muted"}>Muted</Button>
-				<Button variant={"tertiary"}>Tertiary</Button>
-			</div>
-			<div className="space-x-4">
-				<Button>Default</Button>
-				<Button size="lg">Large</Button>
-				<Button size="sm">Small</Button>
-				<Button size={"xs"}>Extra Small</Button>
-				<Button size="icon">
-					<MessageCircleIcon />
-				</Button>
-			</div>
-			<div>
-				<Button disabled>Disabled</Button>
-			</div>
-			<div>
-				<Input />
-			</div>
-		</div>
-	);
+  return <div className="space-y-4 p-4">HomePage</div>;
 };
 
 export default HomePage;
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,17 +1,47 @@
-import { Hono } from "hono";
-import { zValidator } from "@hono/zod-validator";
+import { AUTH_COOKIE } from "@/features/auth/constants";
+import { createAdminClient } from "@/lib/appwrite";
 import { signInSchema, signUpSchema } from "@/lib/validation";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { setCookie } from "hono/cookie";
+import { ID } from "node-appwrite";
 
 const app = new Hono()
   .post("/login", zValidator("json", signInSchema), async (c) => {
     const { email, password } = await c.req.valid("json");
 
-    return c.json({ email, password });
+    const { account } = await createAdminClient();
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   })
   .post("/register", zValidator("json", signUpSchema), async (c) => {
     const { email, name, password } = await c.req.valid("json");
 
-    return c.json({ success: "ok" });
+    const { account } = await createAdminClient();
+
+    const user = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ data: user });
   });
 
 export default app;
