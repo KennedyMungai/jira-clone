c5550fafc53ca82281ed128ca617004f457f5151
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 08:38:47 +0300

    Closing the sidebar when the pathname changes

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -4,10 +4,17 @@ import Sidebar from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => setIsOpen(false), [pathname]);
+
   return (
-    <Sheet modal={false}>
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <Button variant={"secondary"} className="size-8 lg:hidden" size="icon">
           <MenuIcon className="size-5 text-neutral-500" />
