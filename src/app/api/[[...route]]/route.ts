b5ee87dd190693551495ad994547e0199c27eb59
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 08:50:52 +0300

    Created and registered the workspaces endpoint

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,14 @@
+import auth from "@/features/auth/server/route";
+import workspaces from "@/features/workspaces/server/route";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import auth from "@/features/auth/server/route";
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api").route("/auth", auth);
+const app = new Hono()
+  .basePath("/api")
+  .route("/auth", auth)
+  .route("/workspaces", workspaces);
 
 export const GET = handle(app);
 export const POST = handle(app);
