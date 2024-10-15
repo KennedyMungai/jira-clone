2768ccec5d8c47615a25ae4b469044570483d975
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 09:51:10 +0300

    Added toast messges for successfully creating a message or an error

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,6 +1,7 @@
 import { client } from "@/lib/hc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<typeof client.api.workspaces.$post>;
 type RequestType = InferRequestType<typeof client.api.workspaces.$post>["json"];
@@ -14,8 +15,11 @@ export const useCreateWorkspace = () => {
 
       return await response.json();
     },
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: ["workspaces"] }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      toast.success("Workspace created successfully");
+    },
+    onError: () => toast.error("Something went wrong"),
   });
 
   return mutation;
