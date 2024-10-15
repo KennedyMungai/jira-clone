b6814c8a8937da1752f3cd1aa8bfb43b44540df9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 15:47:59 +0300

    Implemented the useCreateWorkspaceModal hook

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -2,10 +2,13 @@
 
 import ResponsiveModal from "@/components/responsive-modal";
 import CreateWorkspaceForm from "@/features/workspaces/components/create-workspace-form";
+import { useCreateWorkspaceModal } from "@/features/workspaces/hooks/use-create-workspace-modal";
 
 const CreateWorkspaceModal = () => {
+  const { isOpen, setIsOpen } = useCreateWorkspaceModal();
+
   return (
-    <ResponsiveModal open onOpenChange={() => {}}>
+    <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
       <CreateWorkspaceForm />
     </ResponsiveModal>
   );
