3ad8afc77ed9220f97969c64f506d72e93d647ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 12:07:14 +0300

    A barebones workspace switcher

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,7 +1,43 @@
 "use client";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { RiAddCircleFill } from "react-icons/ri";
+
 const WorkspaceSwitcher = () => {
-  return <div>WorkspaceSwitcher</div>;
+  const { data: workspaces, isPending } = useGetWorkspaces();
+
+  if (isPending) {
+    return <Skeleton className="size-full" />;
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+        <RiAddCircleFill className="size-5 cursor-pointer text-neutral-500 transition hover:opacity-75" />
+      </div>
+      <Select>
+        <SelectTrigger className="w-full bg-neutral-200 p-1 font-medium">
+          <SelectValue placeholder="No workspace selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.documents.map((workspace) => (
+            <SelectItem key={workspace.$id} value={workspace.$id}>
+              {workspace.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default WorkspaceSwitcher;
