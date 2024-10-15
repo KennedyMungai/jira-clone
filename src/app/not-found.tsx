da35db3504eb278333c158b0c6872b6083ba39c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-15 08:11:15 +0300

    Updated the background color of the not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex size-full flex-col items-center justify-center gap-y-4 bg-neutral-200">
+    <div className="flex size-full flex-col items-center justify-center gap-y-4 bg-neutral-100">
       <Link href="/">
         <Image src="/logo.svg" width={164} height={48} alt="logo" />
       </Link>
