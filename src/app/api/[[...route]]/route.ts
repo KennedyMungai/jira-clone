5c29cffc10af055d1b5e0117b0811b945b946ec7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 15:50:28 +0300

    Defined and updated the members api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,4 +1,5 @@
 import auth from "@/features/auth/server/route";
+import members from "@/features/members/server/route";
 import workspaces from "@/features/workspaces/server/route";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
@@ -8,7 +9,8 @@ export const runtime = "edge";
 const app = new Hono()
   .basePath("/api")
   .route("/auth", auth)
-  .route("/workspaces", workspaces);
+  .route("/workspaces", workspaces)
+  .route("/members", members);
 
 export const GET = handle(app);
 export const POST = handle(app);
