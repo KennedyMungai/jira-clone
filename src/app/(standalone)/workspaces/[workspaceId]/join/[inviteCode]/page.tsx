5320991e0981094cb5acae25984bf0f363e8fd8c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 15:05:12 +0300

    Created the workspace join page

---
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrent } from "@/features/auth/queries";
+import JoinWorkspaceForm from "@/features/workspaces/components/join-workspace-form";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+import { redirect } from "next/navigation";
+
+type Props = {
+  params: {
+    workspaceId: string;
+    inviteCode: string;
+  };
+};
+
+const WorkspaceJoinPage = async ({
+  params: { inviteCode, workspaceId },
+}: Props) => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getWorkspaceInfo({ workspaceId });
+
+  if (!initialValues) redirect("/");
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <JoinWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default WorkspaceJoinPage;
