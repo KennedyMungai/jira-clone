052c459215badef233ecd4400446269ebf04fc0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 10:50:07 +0300

    Defined the api hook to delete workspaces

---
+++ src/features/workspaces/api/use-delete-workspace.ts
@@ -0,0 +1,34 @@
+import { client } from "@/lib/hc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[":workspaceId"]["$delete"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["$delete"]
+>;
+
+export const useDeleteWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.workspaces[":workspaceId"]["$delete"]({
+        param,
+      });
+
+      if (!response.ok) throw new Error("Failed to delete workspace");
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      toast.success("Workspace deleted successfully");
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return mutation;
+};
