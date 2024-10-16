d47733015a12276a671cd38adff99aeabdeb02a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 12:17:00 +0300

    Defined the endpoint to update the workspace invite code

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -144,6 +144,38 @@ const app = new Hono()
       return c.json({ data: workspace });
     },
   )
+  .post(
+    "/:workspaceId/reset-invite-code",
+    zValidator("param", z.object({ workspaceId: z.string() })),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const user = c.get("user");
+
+      const { workspaceId } = c.req.valid("param");
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member || member.role !== MemberRole.ADMIN) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const workspace = await databases.updateDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+        {
+          inviteCode: generateInviteCode({ length: 6 }),
+        },
+      );
+
+      return c.json({ data: workspace });
+    },
+  )
   .delete(
     "/:workspaceId",
     zValidator("param", z.object({ workspaceId: z.string() })),
