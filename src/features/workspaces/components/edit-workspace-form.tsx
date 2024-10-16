69e69359cc7047c9704a933b95aa892b76b4dde4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 09:25:36 +0300

    Created the edit workspace form component

---
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import DottedSeparator from "@/components/dotted-separator";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace";
+import {
+  createWorkspaceSchema,
+  updateWorkspaceSchema,
+} from "@/features/workspaces/schema";
+import { Workspace } from "@/features/workspaces/types";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ImageIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type Props = {
+  onCancel?: () => void;
+  initialValues: Workspace;
+};
+
+const EditWorkspaceForm = ({ onCancel, initialValues }: Props) => {
+  const { mutate, isPending } = useUpdateWorkspace();
+
+  const router = useRouter();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const form = useForm<z.infer<typeof updateWorkspaceSchema>>({
+    resolver: zodResolver(updateWorkspaceSchema),
+    defaultValues: {
+      ...initialValues,
+      image: initialValues.imageUrl ?? "",
+    },
+  });
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      form.setValue("image", file);
+    }
+  };
+
+  const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : undefined,
+    };
+
+    mutate(
+      {
+        form: finalValues,
+        param: { workspaceId: initialValues.$id },
+      },
+      {
+        onSuccess: ({ data }) => router.push(`/workspaces/${data.$id}`),
+        onSettled: () => {
+          form.reset();
+        },
+      },
+    );
+  };
+
+  return (
+    <Card className="size-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">
+          {initialValues.name}
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-lg font-semibold text-neutral-500">
+                      Workspace Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter workspace name"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <div className="flex flex-col gap-y-2">
+                    <div className="flex items-center gap-x-5">
+                      {field.value ? (
+                        <div className="relative size-[72px] overflow-hidden rounded-md">
+                          <Image
+                            src={
+                              field.value instanceof File
+                                ? URL.createObjectURL(field.value)
+                                : field.value
+                            }
+                            fill
+                            className="object-cover"
+                            alt="workspace-logo"
+                          />
+                        </div>
+                      ) : (
+                        <Avatar className="size-[72px]">
+                          <AvatarFallback>
+                            <ImageIcon className="size-[36px] text-neutral-400" />
+                          </AvatarFallback>
+                        </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <p className="text-sm">Workspace Icon</p>
+                        <p className="text-xs text-muted-foreground">
+                          JPG, PNG, SVG, JPEG, GIF, max 1mb
+                        </p>
+                        <input
+                          type="file"
+                          className="hidden"
+                          accept=".jpg, .jpeg, .png, .svg, .gif"
+                          ref={inputRef}
+                          disabled={isPending}
+                          onChange={handleImageChange}
+                        />
+                        <Button
+                          type="button"
+                          disabled={isPending}
+                          variant={"tertiary"}
+                          size="xs"
+                          className="mt-2 w-fit"
+                          onClick={() => inputRef.current?.click()}
+                        >
+                          Upload Image
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              />
+            </div>
+            <DottedSeparator className="py-7" />
+            <div className="flex flex-col items-center gap-2 lg:flex-row lg:justify-between">
+              <Button
+                type="button"
+                variant="secondary"
+                size="lg"
+                onClick={onCancel}
+                disabled={isPending}
+                className={cn(!onCancel && "invisible")}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" size="lg" disabled={isPending}>
+                Create Workspace
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default EditWorkspaceForm;
