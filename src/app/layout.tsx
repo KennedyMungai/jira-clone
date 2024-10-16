27502a5cf168b741c7c052228a6a0bfea3606bc0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-16 08:48:50 +0300

    Removed the richColors prop from the Toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>{children}</QueryProvider>
-        <Toaster richColors />
+        <Toaster />
       </body>
     </html>
   );
