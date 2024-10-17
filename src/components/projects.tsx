9de87d1816df1950ae9ebc08f32f41f8fb0418d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-17 10:17:29 +0300

    Wrote the basic code for the projects component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,23 @@
 "use client";
 
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useProjectId } from "@/features/projects/hooks/use-project-id";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 
 const Projects = () => {
+  const pathname = usePathname();
+
+  const workspaceId = useWorkspaceId();
+  const projectId = useProjectId();
+
+  const { data: projects, isPending: isFetchingProjects } = useGetProjects({
+    workspaceId,
+  });
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -12,6 +27,24 @@ const Projects = () => {
           onClick={() => console.log("Workspaces clicked!")}
         />
       </div>
+      {projects?.documents.map((project) => {
+        const href = `/workspaces/${workspaceId}/projects/${projectId}`;
+
+        const isActive = pathname === href;
+
+        return (
+          <Link href={href} key={project.$id}>
+            <div
+              className={cn(
+                "flex cursor-pointer items-center gap-2.5 rounded-md p-2.5 text-neutral-500 transition hover:opacity-75",
+                isActive && "bg-white text-primary shadow-sm hover:opacity-100",
+              )}
+            >
+              <span className="truncate">{project.name}</span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
