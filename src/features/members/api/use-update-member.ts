1c2bccdd4a9608755b104bbac01704beeb41a8f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-17 07:58:20 +0300

    Defined the api hook for updating members in the database

---
+++ src/features/members/api/use-update-member.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/hc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.members)[":memberId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.members)[":memberId"]["$patch"]
+>;
+
+export const useUpdateMember = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const response = await client.api.members[":memberId"]["$patch"]({
+        param,
+        json,
+      });
+
+      if (!response.ok) throw new Error("Failed to delete member");
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Member deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["members"] });
+    },
+    onError: () => toast.error("Failed to delete member"),
+  });
+
+  return mutation;
+};
