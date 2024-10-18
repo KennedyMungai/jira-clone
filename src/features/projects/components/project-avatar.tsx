e204ea23b08ae62b5e02470c30b83707d2359774
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-18 12:10:38 +0300

    Updated some styles inside the Project Avatar compoent

--- src/features/projects/components/project-avatar.tsx
+++ src/features/projects/components/project-avatar.tsx
@@ -5,20 +5,31 @@ type Props = {
   image?: string;
   name: string;
   className?: string;
+  fallbackClassName?: string;
 };
 
-const ProjectAvatar = ({ name, className, image }: Props) => {
+const ProjectAvatar = ({
+  name,
+  className,
+  image,
+  fallbackClassName,
+}: Props) => {
   if (image) {
     return (
       <div
-        className={cn("relative size-10 overflow-hidden rounded-md", className)}
+        className={cn("relative size-5 overflow-hidden rounded-md", className)}
       ></div>
     );
   }
 
   return (
-    <Avatar className={cn("size-10 rounded-md", className)}>
-      <AvatarFallback className="bg-blue-600 text-lg font-semibold uppercase text-white">
+    <Avatar className={cn("size-5 rounded-md", className)}>
+      <AvatarFallback
+        className={cn(
+          "bg-blue-600 text-sm font-semibold text-white",
+          fallbackClassName,
+        )}
+      >
         {name[0]}
       </AvatarFallback>
     </Avatar>
