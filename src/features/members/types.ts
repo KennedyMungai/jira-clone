7aff0e1ee07f9e1fc5a33519f9356d33d4196690
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-18 14:46:56 +0300

    Added the memberrole to the enums

--- src/features/members/types.ts
+++ src/features/members/types.ts
@@ -1,5 +1,10 @@
 import { Models } from "node-appwrite";
 
+export enum MemberRole {
+  ADMIN = "admin",
+  MEMBER = "member",
+}
+
 export type Project = Models.Document & {
   name: string;
   imageUrl: string;
