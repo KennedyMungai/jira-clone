eea690e32a7109ecfc0817a410617e5a4c621b7c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-18 14:58:15 +0300

    Created the project settings page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/edit/page.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  params: {
+    workspaceId: string;
+    projectId: string;
+  };
+};
+
+const ProjectEditPage = ({ params: { workspaceId, projectId } }: Props) => {
+  return <div>ProjectEditPage</div>;
+};
+
+export default ProjectEditPage;
