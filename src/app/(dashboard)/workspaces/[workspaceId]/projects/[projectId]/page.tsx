65cd03abb83f0b0ee8d03f639c75d99563c91799
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-18 14:55:40 +0300

    Added the edit button

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
+import ProjectAvatar from "@/features/projects/components/project-avatar";
 import { getProject } from "@/features/projects/queries";
+import { EditIcon, Link } from "lucide-react";
 import { redirect } from "next/navigation";
 
 type Props = {
@@ -18,7 +21,36 @@ const ProjectPage = async ({ params: { projectId, workspaceId } }: Props) => {
     projectId,
   });
 
-  return <div>{JSON.stringify(initialValues)}</div>;
+  if (!initialValues) throw new Error("Project not found!");
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={initialValues.name}
+            image={initialValues.image}
+            className={"size-8"}
+          />
+          <p className="text-lg font-semibold">{initialValues.name}</p>
+        </div>
+        <div>
+          <Button
+            size="icon"
+            variant="ghost"
+            className="text-neutral-500"
+            asChild
+          >
+            <Link
+              href={`/workspaces/${workspaceId}/projects/${projectId}/edit`}
+            >
+              <EditIcon className="size-5" />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectPage;
