0f4be1982718dffba5e5442442f2afc7837416c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-18 14:25:17 +0300

    Defined the workspaceId and the projectId on the project page

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,5 +1,17 @@
-const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+type Props = {
+  params: {
+    workspaceId: string;
+    projectId: string;
+  };
+};
+
+const ProjectPage = ({ params: { projectId, workspaceId } }: Props) => {
+  return (
+    <div>
+      <p>Project Id: {projectId}</p>
+      <p>Workspace Id: {workspaceId}</p>
+    </div>
+  );
 };
 
 export default ProjectPage;
