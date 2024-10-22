ac98d884b53214852c6c111a1a3e3326da6cd0d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 15:42:29 +0300

    Created and rendered the task date component

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import TaskDate from "@/features/tasks/components/task-date";
 import { Task } from "@/features/tasks/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDownIcon } from "lucide-react";
@@ -16,5 +17,66 @@ export const columns: ColumnDef<Task>[] = [
         Task Name <ArrowUpDownIcon className="ml-2 size-4" />
       </Button>
     ),
+    cell: ({ row }) => {
+      const name = row.original.name;
+
+      return <p className="line-clamp-1">{name}</p>;
+    },
+  },
+  {
+    accessorKey: "project",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Project <ArrowUpDownIcon className="ml-2 size-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const project = row.original.project;
+
+      return (
+        <div className="flex items-center gap-x-2 text-sm font-medium">
+          <p className="line-clamp-1">{project.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "assignee",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Assignee <ArrowUpDownIcon className="ml-2 size-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const assignee = row.original.assignee;
+
+      return (
+        <div className="flex items-center gap-x-2 text-sm font-medium">
+          <p className="line-clamp-1 text-muted-foreground">{assignee.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "dueDate",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Due Date <ArrowUpDownIcon className="ml-2 size-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const dueDate = row.original.dueDate;
+
+      return <TaskDate value={dueDate} />;
+    },
   },
 ];
