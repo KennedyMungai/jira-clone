19281a05df5f46023d7f5fec5af23ccfb6f91e51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 14:29:24 +0300

    Added the project and assignee filters

--- src/features/tasks/components/data-filters.tsx
+++ src/features/tasks/components/data-filters.tsx
@@ -10,9 +10,10 @@ import {
 } from "@/components/ui/select";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useTaskFilters } from "@/features/tasks/hooks/use-task-filters";
 import { TaskStatus } from "@/features/tasks/types";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
-import { ListChecksIcon } from "lucide-react";
+import { FolderIcon, ListChecksIcon, UserIcon } from "lucide-react";
 
 type Props = {
   hideProjectFilter?: boolean;
@@ -40,15 +41,30 @@ const DataFilters = ({ hideProjectFilter }: Props) => {
     label: member.name,
   }));
 
+  const [{ assigneeId, dueDate, projectId, search, status }, setFilters] =
+    useTaskFilters();
+
+  const onStatusChange = (value: string) =>
+    setFilters({ status: value === "all" ? null : (value as TaskStatus) });
+
+  const onAssigneeChange = (value: string) =>
+    setFilters({ assigneeId: value === "all" ? null : (value as string) });
+
+  const onProjectChange = (value: string) =>
+    setFilters({ projectId: value === "all" ? null : (value as string) });
+
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col gap-2 lg:flex-row">
-      <Select defaultValue={undefined} onValueChange={() => {}}>
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
         <SelectTrigger disabled={isLoading} className="h-8 w-full lg:w-auto">
           <div className="flex items-center pr-2">
             <ListChecksIcon className="mr-2 size-4" />
-            <SelectValue placeholder="All Statuses" />
+            <SelectValue placeholder="All statuses" />
           </div>
         </SelectTrigger>
         <SelectContent>
@@ -59,6 +75,46 @@ const DataFilters = ({ hideProjectFilter }: Props) => {
           <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
           <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
           <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+        </SelectContent>
+      </Select>
+      <Select
+        defaultValue={assigneeId ?? undefined}
+        onValueChange={(value) => onAssigneeChange(value)}
+      >
+        <SelectTrigger disabled={isLoading} className="h-8 w-full lg:w-auto">
+          <div className="flex items-center pr-2">
+            <UserIcon className="mr-2 size-4" />
+            <SelectValue placeholder="All assignees" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All assignees</SelectItem>
+          <SelectSeparator />
+          {memberOptions?.map((member) => (
+            <SelectItem key={member.value} value={member.value}>
+              {member.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select
+        defaultValue={projectId ?? undefined}
+        onValueChange={(value) => onProjectChange(value)}
+      >
+        <SelectTrigger disabled={isLoading} className="h-8 w-full lg:w-auto">
+          <div className="flex items-center pr-2">
+            <FolderIcon className="mr-2 size-4" />
+            <SelectValue placeholder="All projects" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All projects</SelectItem>
+          <SelectSeparator />
+          {projectOptions?.map((project) => (
+            <SelectItem key={project.value} value={project.value}>
+              {project.label}
+            </SelectItem>
+          ))}
         </SelectContent>
       </Select>
     </div>
