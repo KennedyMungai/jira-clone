b4ad1ff61134a2fafcb129f055613f69c753c69a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 12:25:19 +0300

    Added some additional props to the useGetTasks hook

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -1,16 +1,44 @@
+import { TaskStatus } from "@/features/tasks/types";
 import { client } from "@/lib/hc";
 import { useQuery } from "@tanstack/react-query";
 
 type Props = {
   workspaceId: string;
+  projectId?: string | null;
+  status?: TaskStatus | null;
+  assigneeId?: string | null;
+  dueDate?: string | null;
+  search?: string | null;
 };
 
-export const useGetTasks = ({ workspaceId }: Props) =>
+export const useGetTasks = ({
+  workspaceId,
+  assigneeId,
+  dueDate,
+  status,
+  projectId,
+  search,
+}: Props) =>
   useQuery({
-    queryKey: ["tasks", workspaceId],
+    queryKey: [
+      "tasks",
+      workspaceId,
+      projectId,
+      status,
+      assigneeId,
+      dueDate,
+      search,
+    ],
     queryFn: async () => {
       const response = await client.api.tasks.$get({
-        query: { workspaceId },
+        query: {
+          workspaceId,
+          projectId: projectId ?? undefined,
+          status: status ?? undefined,
+          assigneeId: assigneeId ?? undefined,
+          search: search ?? undefined,
+          dueDate: dueDate ?? undefined,
+        },
       });
 
       if (!response.ok) throw new Error("Failed to fetch tasks");
