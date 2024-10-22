30d746df036514a8a7ed92836debba868d11a0d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 09:34:13 +0300

    Implemented some bqsic tab functionality

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,5 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const TaskViewSwitcher = () => {
-  return <div>TaskViewSwitcher</div>;
+  return (
+    <Tabs className="w-full flex-1 rounded-lg border">
+      <div className="flex h-full flex-col overflow-auto p-4">
+        <div className="flex flex-col items-center justify-between gap-y-2 lg:flex-row">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value={"table"}>
+              Table
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value={"kanban"}>
+              Kanban
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value={"calendar"}>
+              Calendar
+            </TabsTrigger>
+          </TabsList>
+        </div>
+      </div>
+    </Tabs>
+  );
 };
 
 export default TaskViewSwitcher;
