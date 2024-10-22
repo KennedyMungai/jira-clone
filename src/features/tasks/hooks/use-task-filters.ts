de5ce8360ce1ffa89edc452fffce8bef414bdb58
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 14:02:41 +0300

    Exported the useTaskFilters hook

--- src/features/tasks/hooks/use-task-filters.ts
+++ src/features/tasks/hooks/use-task-filters.ts
@@ -1,7 +1,7 @@
 import { parseAsString, parseAsStringEnum, useQueryStates } from "nuqs";
 import { TaskStatus } from "@/features/tasks/types";
 
-const useTaskFilters = () =>
+export const useTaskFilters = () =>
   useQueryStates({
     projectId: parseAsString,
     status: parseAsStringEnum(Object.values(TaskStatus)),
