2b999c4a765230b6f99a865c7c47bc4d8e7e5abc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-22 15:17:02 +0300

    Added a button on the header for the sorting functionality

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -2,10 +2,19 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "@/features/tasks/types";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDownIcon } from "lucide-react";
 
 export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "name",
-    header: "Task Name",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Name <ArrowUpDownIcon className="ml-2 size-4" />
+      </Button>
+    ),
   },
 ];
