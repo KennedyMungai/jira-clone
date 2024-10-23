6e395f623ce1fb3b4b3dbfb30ac07b4c1087681c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-23 10:30:21 +0300

    Created and rendered the Dtaa Kanban component

---
+++ src/features/tasks/components/data-kanban.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Task } from "@/features/tasks/types";
+
+type Props = {
+  data: Task[];
+};
+
+const DataKanban = ({ data }: Props) => {
+  return <div>DataKanban</div>;
+};
+
+export default DataKanban;
