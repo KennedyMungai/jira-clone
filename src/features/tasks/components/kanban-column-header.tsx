9f5a6fb18415fa3815c7347d19ba7e30cc65b18d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-23 11:18:48 +0300

    Added icons and a button to add tasks

--- src/features/tasks/components/kanban-column-header.tsx
+++ src/features/tasks/components/kanban-column-header.tsx
@@ -1,20 +1,57 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
 import { TaskStatus } from "@/features/tasks/types";
 import { snakeCaseToTitleCase } from "@/lib/utils";
+import {
+  CircleCheckIcon,
+  CircleDashedIcon,
+  CircleDotDashedIcon,
+  CircleDotIcon,
+  CircleIcon,
+  PlusIcon,
+} from "lucide-react";
+import { ReactNode } from "react";
 
 type Props = {
   board: TaskStatus;
   taskCount: number;
 };
 
+const statusIconMap: Record<TaskStatus, ReactNode> = {
+  [TaskStatus.BACKLOG]: (
+    <CircleDashedIcon className="size-[18px] text-pink-400" />
+  ),
+  [TaskStatus.TODO]: <CircleIcon className="size-[18px] text-red-400" />,
+  [TaskStatus.IN_PROGRESS]: (
+    <CircleDotDashedIcon className="size-[18px] text-yellow-400" />
+  ),
+  [TaskStatus.IN_REVIEW]: (
+    <CircleDotIcon className="size-[18px] text-blue-400" />
+  ),
+  [TaskStatus.DONE]: (
+    <CircleCheckIcon className="size-[18px] text-emerald-400" />
+  ),
+};
+
 const KanbanColumnHeader = ({ board, taskCount }: Props) => {
+  const { open } = useCreateTaskModal();
+
+  const icon = statusIconMap[board];
+
   return (
     <div className="flex items-center justify-between px-2 py-1.5">
       <div className="flex items-center gap-x-2">
-        <h2>{snakeCaseToTitleCase(board)}</h2>
-        <div>{taskCount}</div>
+        {icon}
+        <h2 className="text-sm font-medium">{snakeCaseToTitleCase(board)}</h2>
+        <div className="flex size-5 items-center justify-center rounded-md bg-neutral-200 text-xs font-medium text-neutral-700">
+          {taskCount}
+        </div>
       </div>
+      <Button size="icon" variant={"ghost"} onClick={open}>
+        <PlusIcon className="size-5 text-neutral-500" />
+      </Button>
     </div>
   );
 };
