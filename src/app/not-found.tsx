c230dea4464b785df8d905f2c97d5efe5133ea09
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-23 08:39:28 +0300

    Updated the not found page for the project

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,12 @@ const NotFoundPage = () => {
   return (
     <div className="flex size-full flex-col items-center justify-center gap-y-4 bg-neutral-100">
       <Link href="/">
-        <Image src="/logo.svg" width={164} height={48} alt="logo" />
+        <Image src="/logo.svg" width={328} height={96} alt="logo" />
       </Link>
+
+      <h2 className="text-2xl font-bold text-muted-foreground">
+        Page Not Found
+      </h2>
       <Button asChild>
         <Link href="/">Go Back</Link>
       </Button>
