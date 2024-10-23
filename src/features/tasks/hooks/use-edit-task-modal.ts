8300d165b1b514f15c5f7362f8a0bbd5c43ef4f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-23 08:58:26 +0300

    Defined the use edit task hook

---
+++ src/features/tasks/hooks/use-edit-task-modal.ts
@@ -0,0 +1,18 @@
+import { parseAsBoolean, useQueryState } from "nuqs";
+
+export const useEditTaskModal = () => {
+  const [isOpen, setIsOpen] = useQueryState(
+    "edit-task",
+    parseAsBoolean.withDefault(false).withOptions({ clearOnDefault: true }),
+  );
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  return {
+    isOpen,
+    open,
+    close,
+    setIsOpen,
+  };
+};
