531b86712f62a3366591020b7b64bcf552f1033a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 07:38:12 +0300

    Used the proper data types for the props

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Project } from "@/features/members/types";
+import { TaskStatus } from "@/features/tasks/types";
+
 type Props = {
   id: string;
   title: string;
-  assignee: string;
-  project: string;
-  status: string;
+  assignee: unknown;
+  project: Project;
+  status: TaskStatus;
 };
 
 const EventCard = ({ assignee, id, project, status, title }: Props) => {
