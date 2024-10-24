2e4029b4feaba14315e69aacdb76fa02e35c497c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 12:47:08 +0300

    Cleared some of the server action functions from the query file

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,6 +1,4 @@
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
-import { getMember } from "@/features/members/utils";
-import { Workspace } from "@/features/workspaces/types";
 import { createSessionClient } from "@/lib/appwrite";
 import { Query } from "node-appwrite";
 
@@ -25,50 +23,4 @@ export const getWorkspaces = async () => {
   ]);
 
   return workspaces;
-};
-
-type GetWorkspaceProps = {
-  workspaceId: string;
-};
-
-export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
-  const { account, databases } = await createSessionClient();
-
-  const user = await account.get();
-
-  const member = await getMember({
-    databases,
-    userId: user.$id,
-    workspaceId,
-  });
-
-  if (!member) throw new Error("Unauthorized");
-
-  const workspace = await databases.getDocument<Workspace>(
-    DATABASE_ID,
-    WORKSPACES_ID,
-    workspaceId,
-  );
-
-  return workspace;
-};
-
-type GetWorkspaceInfoProps = {
-  workspaceId: string;
-};
-
-export const getWorkspaceInfo = async ({
-  workspaceId,
-}: GetWorkspaceInfoProps) => {
-  const { databases } = await createSessionClient();
-
-  const workspace = await databases.getDocument<Workspace>(
-    DATABASE_ID,
-    WORKSPACES_ID,
-    workspaceId,
-  );
-
-  return {
-    name: workspace.name,
-  };
 };
