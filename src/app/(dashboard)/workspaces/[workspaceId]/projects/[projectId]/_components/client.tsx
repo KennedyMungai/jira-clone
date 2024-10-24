65f25198568aeb98731b89966561ac1407779633
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 10:50:16 +0300

    Created and rendered the Project Details component

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/_components/client.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import PageError from "@/components/page-error";
+import PageLoader from "@/components/page-loader";
+import { Button } from "@/components/ui/button";
+import { useGetProject } from "@/features/projects/api/use-get-project";
+import ProjectAvatar from "@/features/projects/components/project-avatar";
+import { useProjectId } from "@/features/projects/hooks/use-project-id";
+import TaskViewSwitcher from "@/features/tasks/components/task-view-switcher";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { EditIcon } from "lucide-react";
+import Link from "next/link";
+
+const ProjectDetailsClient = () => {
+  const projectId = useProjectId();
+  const workspaceId = useWorkspaceId();
+
+  const { data: project, isPending: isProjectPending } = useGetProject({
+    projectId,
+  });
+
+  if (isProjectPending) return <PageLoader />;
+
+  if (!project) return <PageError message="Project not found" />;
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={project.name}
+            image={project.image}
+            className={"size-8"}
+          />
+          <p className="text-lg font-semibold">{project.name}</p>
+        </div>
+        <div>
+          <Button
+            size="icon"
+            variant="ghost"
+            className="text-neutral-500"
+            asChild
+          >
+            <Link
+              href={`/workspaces/${workspaceId}/projects/${projectId}/edit`}
+            >
+              <EditIcon className="size-5" />
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <TaskViewSwitcher hideProjectFilter />
+    </div>
+  );
+};
+
+export default ProjectDetailsClient;
--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,59 +1,13 @@
-import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
-import ProjectAvatar from "@/features/projects/components/project-avatar";
-import { getProject } from "@/features/projects/queries";
-import TaskViewSwitcher from "@/features/tasks/components/task-view-switcher";
-import { EditIcon } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
+import ProjectDetailsClient from "./_components/client";
 
-type Props = {
-  params: {
-    workspaceId: string;
-    projectId: string;
-  };
-};
-
-const ProjectPage = async ({ params: { projectId, workspaceId } }: Props) => {
+const ProjectPage = async () => {
   const user = await getCurrent();
 
   if (!user) redirect("/sign-in");
 
-  const initialValues = await getProject({
-    projectId,
-  });
-
-  if (!initialValues) throw new Error("Project not found!");
-
-  return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-2">
-          <ProjectAvatar
-            name={initialValues.name}
-            image={initialValues.image}
-            className={"size-8"}
-          />
-          <p className="text-lg font-semibold">{initialValues.name}</p>
-        </div>
-        <div>
-          <Button
-            size="icon"
-            variant="ghost"
-            className="text-neutral-500"
-            asChild
-          >
-            <Link
-              href={`/workspaces/${workspaceId}/projects/${projectId}/edit`}
-            >
-              <EditIcon className="size-5" />
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <TaskViewSwitcher hideProjectFilter />
-    </div>
-  );
+  return <ProjectDetailsClient />;
 };
 
 export default ProjectPage;
