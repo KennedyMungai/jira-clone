ffa30bd7961a121ac593c96eebdd4d00b6264cfe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 15:20:26 +0300

    Created and rendered the analytics card

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
+import AnalyticsCard from "@/components/analytics-card";
 
 const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
   if (!data) return null;
 
-  return <div>Analytics</div>;
+  return (
+    <ScrollArea className="w-full shrink-0 whitespace-nowrap rounded-lg border">
+      <div className="flex w-full flex-row">
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title={"Total tasks"}
+            value={data.tasksCount}
+            variant={data.tasksDifference > 0 ? "up" : "down"}
+            increaseValue={data.tasksDifference}
+          />
+        </div>
+      </div>
+    </ScrollArea>
+  );
 };
 
 export default Analytics;
