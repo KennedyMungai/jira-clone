76608b1da118891c531556f7d098c229a51a3d4e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 15:41:20 +0300

    Added all 5 analytics cards

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import AnalyticsCard from "@/components/analytics-card";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
+import DottedSeparator from "./dotted-separator";
 
 const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
   if (!data) return null;
@@ -17,8 +18,45 @@ const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
             variant={data.tasksDifference > 0 ? "up" : "down"}
             increaseValue={data.tasksDifference}
           />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title={"Assigned Tasks"}
+            value={data.assignedTasksCount}
+            variant={data.assignedTasksDifference > 0 ? "up" : "down"}
+            increaseValue={data.assignedTasksDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title={"Completed Tasks"}
+            value={data.completedTasksCount}
+            variant={data.completedTasksDifference > 0 ? "up" : "down"}
+            increaseValue={data.completedTasksDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title={"Overdue Tasks"}
+            value={data.overdueTasksCount}
+            variant={data.overdueTasksDifference > 0 ? "up" : "down"}
+            increaseValue={data.overdueTasksDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title={"Incomplete Tasks"}
+            value={data.incompleteTasksCount}
+            variant={data.incompleteTasksDifference > 0 ? "up" : "down"}
+            increaseValue={data.incompleteTasksDifference}
+          />
         </div>
       </div>
+      <ScrollBar orientation="horizontal" />
     </ScrollArea>
   );
 };
