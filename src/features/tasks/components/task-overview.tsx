10909d9c5192bb0e9873edf528249b56c674c922
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 09:52:28 +0300

    Renderred out the task data inside overview components

--- src/features/tasks/components/task-overview.tsx
+++ src/features/tasks/components/task-overview.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import DottedSeparator from "@/components/dotted-separator";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import OverviewProperty from "@/features/tasks/components/overview-property";
+import TaskDate from "@/features/tasks/components/task-date";
 import { Task } from "@/features/tasks/types";
+import { snakeCaseToTitleCase } from "@/lib/utils";
 import { PencilIcon } from "lucide-react";
 
 type Props = {
@@ -21,7 +26,21 @@ const TaskOverview = ({ task }: Props) => {
         </div>
         <DottedSeparator className="my-4" />
         <div className="flex flex-col gap-y-4">
-          <OverviewProperty />
+          <OverviewProperty label="Assignee">
+            <div className="flex items-center gap-x-2">
+              <p className="line-clamp-1 text-muted-foreground">
+                {task.assignee.name}
+              </p>
+            </div>
+          </OverviewProperty>
+          <OverviewProperty label="Due Date">
+            <TaskDate value={task.dueDate} className="text-sm font-medium" />
+          </OverviewProperty>
+          <OverviewProperty label="Status">
+            <Badge variant={task.status}>
+              {snakeCaseToTitleCase(task.status)}
+            </Badge>
+          </OverviewProperty>
         </div>
       </div>
     </div>
