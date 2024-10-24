3125cb4794e107c2629939d51357d76b77546ab7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 07:32:44 +0300

    Added the calendar component

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -1,13 +1,78 @@
 "use client";
 
+import {
+  format,
+  getDay,
+  parse,
+  startOfWeek,
+  addMonths,
+  subMonths,
+} from "date-fns";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import { Task } from "@/features/tasks/types";
+import { enUS } from "date-fns/locale";
+import { useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./data-calendar.css";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 type Props = {
   data: Task[];
 };
 
 const DataCalendar = ({ data }: Props) => {
-  return <div>DataCalendar</div>;
+  const [value, setValue] = useState(
+    data.length > 0 ? new Date(data[0].dueDate) : new Date(),
+  );
+
+  const events = data.map((task) => ({
+    start: new Date(task.dueDate),
+    end: new Date(task.dueDate),
+    title: task.name,
+    project: task.project,
+    assignee: task.assignee,
+    status: task.status,
+    id: task.$id,
+  }));
+
+  const handleNavigate = (action: "PREV" | "NEXT" | "TODAY") => {
+    if (action === "PREV") {
+      setValue(subMonths(value, 1));
+    } else if (action === "NEXT") {
+      setValue(addMonths(value, 1));
+    } else if (action === "TODAY") {
+      setValue(new Date());
+    }
+  };
+
+  return (
+    <Calendar
+      localizer={localizer}
+      date={value}
+      events={events}
+      views={["month"]}
+      defaultView="month"
+      toolbar
+      showAllEvents
+      className="h-full"
+      max={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
+      formats={{
+        weekdayFormat: (date, culture, localizer) =>
+          localizer?.format(date, "EEE", culture) ?? "",
+      }}
+    />
+  );
 };
 
 export default DataCalendar;
