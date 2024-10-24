3363b9bf40bf79d039b8da3c2af253ede41139b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-24 08:07:02 +0300

    Added some more markup and logic to the custom toolbar

--- src/features/tasks/components/custom-toolbar.tsx
+++ src/features/tasks/components/custom-toolbar.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+
 type Props = {
   date: Date;
   onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
 };
 
 const CustomToolbar = ({ date, onNavigate }: Props) => {
-  return <div>CustomToolbar</div>;
+  return (
+    <div className="mb-4 flex w-full items-center justify-center gap-x-2 lg:w-auto lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant={"secondary"}
+        size="icon"
+      >
+        <ChevronLeftIcon className="size-4" />
+      </Button>
+      <div className="flex h-8 w-full items-center justify-center rounded-md border border-input px-3 py-2 lg:w-auto">
+        <CalendarIcon className="mr-2 size-4" />
+        <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+      </div>
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant={"secondary"}
+        size="icon"
+      >
+        <ChevronRightIcon className="size-4" />
+      </Button>
+    </div>
+  );
 };
 
 export default CustomToolbar;
