f1f4d079cd705c5c75f7b233d70c3733cc2338c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-10-25 10:53:02 +0300

    Wired up the github login

--- src/features/auth/components/signin-card.tsx
+++ src/features/auth/components/signin-card.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useLogin } from "@/features/auth/api/use-login";
+import { signUpWithGithub } from "@/lib/oauth";
 import { signInSchema } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
@@ -107,6 +108,7 @@ const SignInCard = () => {
           size="lg"
           className="w-full"
           disabled={isPending}
+          onClick={() => signUpWithGithub()}
         >
           <FaGithub size={24} className="mr-2" />
           Login with Github
